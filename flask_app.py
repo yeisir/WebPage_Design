70242a35a35bdae511b928ad371b2d3e95205865
Tu Nombre <yeisirodriguez22@gmail.com>
2024-03-24 10:16:46 -0500

    pag1

--- flask_app.py
+++ flask_app.py
@@ -10,6 +10,7 @@ load_dotenv()
 app = Flask(__name__)
 socketio = SocketIO(app)
 
+
 # Configuración de la base de datos MySQL
 db = mysql.connector.connect(
     host=os.environ.get("DB_HOST"),
@@ -17,6 +18,45 @@ db = mysql.connector.connect(
     password=os.environ.get("DB_PASSWORD"),
     database=os.environ.get("DB_NAME")
 )
+
+@app.route('/tiempo_real')
+def index():
+    return render_template('pag1.html')
+
+@app.route('/recibir_udp', methods=['GET', 'POST'])
+def recibir_udp():
+    if request.method == 'GET':
+        # Si la solicitud es GET, obtener los datos de la consulta
+        latitud = request.args.get('latitud')
+        longitud = request.args.get('longitud')
+        altitud = request.args.get('altitud')
+        timestamp = request.args.get('timestamp')
+    elif request.method == 'POST':
+        # Si la solicitud es POST, obtener los datos del cuerpo JSON
+        data = request.json
+        print("Datos recibidos en la solicitud POST:", data)
+
+        # Extraer los valores de latitud, longitud, altitud y timestamp
+        latitud = data.get('latitud')
+        longitud = data.get('longitud')
+        altitud = data.get('altitud')
+        timestamp = data.get('timestamp')
+    else:
+        # Si la solicitud no es ni GET ni POST, retornar un mensaje de error
+        return 'Método no permitido'
+
+    # Insertar los datos en la base de datos MySQL
+    cursor = db.cursor()
+    insert_query = "INSERT INTO coordenadas (latitud, longitud, altitud, timestamp) VALUES (%s, %s, %s, %s)"
+    data_tuple = (latitud, longitud, altitud, timestamp)
+    cursor.execute(insert_query, data_tuple)
+    db.commit()
+    cursor.close()
+
+    # Emitir los datos al cliente WebSocket
+    socketio.emit('update_coords', {'latitud': latitud, 'longitud': longitud, 'altitud': altitud, 'timestamp': timestamp})
+    print("Datos enviados al cliente WebSocket:", {'latitud': latitud, 'longitud': longitud, 'altitud': altitud, 'timestamp': timestamp})
+    return 'Datos recibidos y procesados correctamente'
 
 # Función para obtener coordenadas históricas desde la base de datos
 def obtener_coordenadas_historicas(inicio, fin):
@@ -26,10 +66,6 @@ def obtener_coordenadas_historicas(inicio, fin):
     coordenadas_historicas = cursor.fetchall()
     cursor.close()
     return coordenadas_historicas
-
-@app.route('/tiempo_real')
-def tiempo_real():
-    return render_template('pag1.html')
 
 @app.route('/consulta_historica', methods=['GET', 'POST'])
 def consulta_historica():
@@ -54,4 +90,3 @@ def consulta_historica():
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
-
