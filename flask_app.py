37b321d7df48d84febde7de1faa7a56ec2aea4f6
Tu Nombre <yeisirodriguez22@gmail.com>
2024-04-21 21:11:30 -0500

    cambios

--- flask_app.py
+++ flask_app.py
@@ -83,6 +83,13 @@ def consultar_historial():
     # Si no hay valores para inicio y fin, solo muestra la página
     return render_template('pag2.html')
 
+@app.route('/velocidad', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        return render_template('pag3.html')
+    else:
+        return render_template('pag3.html')
+
 
 if __name__ == '__main__':
     socketio.run(app, debug=True, host='0.0.0.0', port=5000)
