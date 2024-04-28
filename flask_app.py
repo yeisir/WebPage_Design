5977fb27ad0d2e3fad110295513c42de097d54e9
Tu Nombre <yeisirodriguez22@gmail.com>
2024-04-27 19:30:58 -0500

    nuevo sniffer

--- flask_app.py
+++ flask_app.py
@@ -58,6 +58,34 @@ def recibir_udp():
     print("Datos enviados al cliente WebSocket:", {'latitud': latitud, 'longitud': longitud, 'altitud': altitud, 'timestamp': timestamp})
     return 'Datos recibidos y procesados correctamente'
 
+#@app.route('/recibir_data', methods=['POST'])
+#def recibir_udp():
+ #   data = request.json
+  #  print("Datos recibidos en la solicitud POST:", data)
+
+    # Extraer los valores de latitud, longitud, altitud y timestamp
+   # latitud = data.get('latitud')
+   # longitud = data.get('longitud')
+    #altitud = data.get('altitud')
+    #timestamp = data.get('timestamp')
+    #speed = data.get('speed')
+    #rpm = data.get('rpm')
+
+    # Insertar los datos en la base de datos MySQL
+    #cursor = db.cursor()
+    #insert_query = "INSERT INTO coordenadas (latitud, longitud, altitud, timestamp, speed, rpm) VALUES (%s, %s, %s, %s, %s, %s)"
+    #data_tuple = (latitud, longitud, altitud, timestamp, speed, rpm)
+    #cursor.execute(insert_query, data_tuple)
+    #db.commit()
+    #cursor.close()
+
+    # Emitir los datos al cliente WebSocket
+    #socketio.emit('update_data', {'latitud': latitud, 'longitud': longitud, 'altitud': altitud, 'timestamp': timestamp, 'speed': speed, 'rpm': rpm})
+    #print("Datos enviados al cliente WebSocket:", {'latitud': latitud, 'longitud': longitud, 'altitud': altitud, 'timestamp': timestamp,  'speed': speed, 'rpm': rpm})
+    #return 'Datos recibidos y procesados correctamente'
+
+
+
 @app.route('/consulta_historica', methods=['POST'])
 def consultar_historial():
     inicio = request.form.get('inicio')
---
+++ sn_data.py
@@ -0,0 +1,41 @@
+import socket
+import requests
+import json
+
+
+def enviar_datos_al_flask(datos):
+    # Formatear los datos
+    latitud, longitud, altitud, timestamp = [line.split(': ')[1] for line in datos.split('\n') if line]
+    datos_formateados = {
+        'latitud': float(latitud),
+        'longitud': float(longitud),
+        'altitud': float(altitud),
+        'timestamp': timestamp,
+        #'speed': float(speed),  
+        #'rpm': float(rpm), 
+    }
+
+    # Convertir a JSON y enviar
+    url = 'http://127.0.0.1:5000/recibir_data'
+    headers = {'Content-Type': 'application/json'}
+    response_post = requests.post(url, json=datos_formateados, headers=headers)
+    print("Respuesta POST:", response_post.text)
+
+def main():
+    host = ''
+    puerto = 8888
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as servidor:
+        servidor.bind((host, puerto))
+        print(f"Servidor UDP escuchando en el puerto {puerto}...")
+
+        while True:
+            datos_recibidos, direccion_cliente = servidor.recvfrom(1024)
+            datos_decodificados = datos_recibidos.decode('utf-8')
+            print("Datos recibidos:", datos_decodificados)
+
+            # Enviar los datos al servidor Flask
+            #enviar_datos_al_flask(datos_decodificados)
+
+if __name__ == "__main__":
+    main()
