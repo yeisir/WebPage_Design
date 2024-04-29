0bae17eba32d08aaa5a2d839486f99b18e153ca0
Tu Nombre <yeisirodriguez22@gmail.com>
2024-04-29 00:12:24 -0500

    DB1

--- sn_data.py
+++ sn_data.py
@@ -35,7 +35,7 @@ def main():
             print("Datos recibidos:", datos_decodificados)
 
             # Enviar los datos al servidor Flask
-            #enviar_datos_al_flask(datos_decodificados)
+            enviar_datos_al_flask(datos_decodificados)
 
 if __name__ == "__main__":
     main()
